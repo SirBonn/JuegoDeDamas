cf891aff867e2691ccc703b44cd2b26006fd8a8c
SirBOn <byronvasquez21@gmail.com>
2021-06-18 12:29:02 -0600

    Se añadieron metodos para la itneraccion del jugador con el juego

--- src/Jugador/ListadoJugadores.java
+++ src/Jugador/ListadoJugadores.java
@@ -1,15 +1,16 @@
 package src.Jugador;
 
 import src.MainDamas.Datos;
+import src.Textos.Escribir;
 
 public class ListadoJugadores {
-    /**Atributos de la clase */
+    /** Atributos de la clase */
     private Jugador jugadores[] = new Jugador[5];
-    private int lugar=0;
+    private int lugar = 0;
 
-    /**Metodos para introducir jugadores a la lista*/
-    public void agregarJugador(String nombre, int edad){
-        if (lugar<5) {
+    /** Metodos para introducir jugadores a la lista */
+    public void agregarJugador(String nombre, int edad) {
+        if (lugar < 5) {
             jugadores[lugar] = new Jugador(nombre, edad);
         } else {
             System.out.println("\n---Limite de jugadores alcanzado---");
@@ -17,33 +18,44 @@ public class ListadoJugadores {
         lugar++;
     }
 
-    public void agregarJugador(){
+    public void agregarJugador() {
         String nombre = Datos.getTexto("Ingrese el nombre del participante: ");
-        int edad = Datos.getEntero("ingrese la edad del participante", false);
+        int edad = Datos.getEntero("Ingrese la edad del participante: ", false);
         agregarJugador(nombre, edad);
     }
 
-    /**Metodo para mostrar los jugadores en el listado*/
-    public void mostrarJugadores(){
-        for (int i=0; i<lugar; i++){
-            System.out.println((i+1)+". " +jugadores[i].getDatosJugador());
+    /** Metodo para mostrar los jugadores en el listado */
+    public void mostrarJugadores() {
+        if (lugar < 6){
+            Escribir.participantes();
+            for (int i = 0; i < lugar; i++) {
+                System.out.println((i + 1) + ". " + jugadores[i].getDatosJugador());
+            }
         }
+
     }
 
-    public void mostrarPosiciones(){
-        
-        for (int i = 1; i < (lugar); i++) {
-            for (int j = 0; j < (lugar-i); j++) {
-                //  String.compareTo(String)
-                //   [-oo , + oo]
-                    boolean ordenar=(jugadores[j].getPartidasGanadas() > jugadores[j+1].getPartidasGanadas());
-                if(ordenar){
-                    //clientes i va antes que clientes j <0 descendente
-                    Jugador aux = jugadores[j];
-                    jugadores[j] = jugadores[j+1];
-                    jugadores[j+1]= aux;
+    public void mostrarPosiciones() {
+
+        if (lugar < 5) {
+            for (int i = 1; i < (lugar); i++) {
+                for (int j = 0; j < (lugar - i); j++) {
+                    // String.compareTo(String)
+                    // [-oo , + oo]
+                    boolean ordenar = (jugadores[j].getPartidasGanadas() > jugadores[j + 1].getPartidasGanadas());
+                    System.out.println((i + 1) + "º lugar: " + jugadores[i].getReportePartidas());
+                    if (ordenar) {
+                        // clientes i va antes que clientes j <0 descendente
+                        Jugador aux = jugadores[j];
+                        jugadores[j] = jugadores[j + 1];
+                        jugadores[j + 1] = aux;
+                    }
                 }
             }
         }
+    }
+
+    public Jugador getJugador(int slot){
+        return jugadores[slot];
     }
 }
