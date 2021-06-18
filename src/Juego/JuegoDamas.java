f9fa61d4483f564a9573dc5a6d709d30902f22b0
SirBOn <byronvasquez21@gmail.com>
2021-06-18 12:30:12 -0600

    se comenzo a añadir metodos para la jugabilidad

--- src/Juego/JuegoDamas.java
+++ src/Juego/JuegoDamas.java
@@ -1,9 +1,33 @@
 package src.Juego;
 
 import src.Jugador.*;
+import src.MainDamas.*;
 
 public class JuegoDamas {
-    
+    private MatrizTablero tablero;
+    private Jugador jugadorBlancas;
+    private Jugador jugadorNegras;
 
+    public JuegoDamas(Jugador jugadorBlancas, Jugador jugadorNegras) {
+        this.tablero = new MatrizTablero();
+        this.jugadorBlancas = jugadorBlancas;
+        this.jugadorNegras = jugadorNegras;
+        dibujarTableroPartida();
+    }
+
+    private void dibujarTableroPartida() {
+        System.out.println("Jugador1: " + jugadorBlancas.getNombre()));
+        System.out.print("_________________________________________________\n");
+        tablero.mostrarTablero();
+        System.out.print("\n_________________________________________________\n");
+        System.out.println("Jugador2: " + jugadorNegras.getNombre());
+    }
+
+
+    public void jugarPartida() {
+        do {
+            
+        } while (jugadorBlancas.getpiezasEnTablero() == 0 || jugadorNegras.getpiezasEnTablero() == 0);
+    }
 
 }
